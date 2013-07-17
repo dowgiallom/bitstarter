607f4e9cafd8dd3f438679ee9a67e23a1a3d1ccb
Maciej Dowgiallo <dowgiallo@gmail.com>
2013-07-17 16:36:06 +0000

    .

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('text');
+  response.send(fs.readFileSync('index.html');
 });
 
 var port = process.env.PORT || 5000;
