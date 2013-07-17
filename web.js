9c53193c54f75259a716dc3d3b36945d7096dbbd
Maciej Dowgiallo <dowgiallo@gmail.com>
2013-07-17 15:34:24 +0000

    added reading from index.html into web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var text = fs.readFileSync('index.html', encoding!g);
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
