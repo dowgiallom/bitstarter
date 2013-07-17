db5764dc3d02ba1d5f2331a62d9ffce965e92c6d
dowgiallom <dowgiallo@gmail.com>
2013-07-17 18:25:32 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var textBuf = fs.readFileSync('index.html');
 var text = textBuf.toString('utf8');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('WTF!?');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
