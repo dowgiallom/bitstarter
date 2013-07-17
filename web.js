bc236fc8e691cb459a16b73bf3c39edd3263702c
Maciej Dowgiallo <dowgiallo@gmail.com>
2013-07-17 16:53:57 +0000

    .

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
-//var textBuf = fs.readFileSync('index.html');
-//var text = textBuf.toString('utf8');
-
 var express = require('express');
 var fs = require('fs');
+
+var textBuf = fs.readFileSync("index.html");
+var text = textBuf.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', encoding);
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
