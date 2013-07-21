a3016d06cea7f87368a983cdd844c42bbee0a15a
Ubuntu <ubuntu@ip-172-31-31-117.us-west-2.compute.internal>
2013-07-21 16:57:40 +0000

    proper web.js finally

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-
-var textBuf = fs.readFileSync("index.html");
-var text = textBuf.toString();
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(text);
+    vat html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
